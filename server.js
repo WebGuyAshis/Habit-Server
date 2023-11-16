460394d5d643a418ee6a37d5f9ec181e54a54d60
Ashis <developer.ashis99@gmail.com>
2023-11-16 13:28:56 +0530

    Added Front End

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import session from "express-session";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+// import path from 'path';
 dotenv.config();
 
 // Using Passport js
@@ -14,14 +17,17 @@ import passportLocal from "./config/passport-local-strategy.js";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // app.use(cors());
 
-const corsOptions = {
-  origin: "https://loquacious-croquembouche-7c162f.netlify.app", // Replace with your frontend URL
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "http://127.0.0.1:3000", // Replace with your frontend URL
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // we basically dont need urlencoded because we are parsing data usning express.json
 // app.use(urlencoded({extended:true}))
@@ -36,9 +42,9 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 24 * 60 * 60 * 2000, // 2 days
-      secure: true,
-      sameSite: "none",
-      domain: '.onrender.com', // Adjust this to your root domain
+      // secure: true,
+      // sameSite: "none",
+      // domain: '.onrender.com',
     },
   })
 );
@@ -56,8 +62,7 @@ app.use(passport.setAuthenticatedUser);
 
 // Setting route
 app.get("/protected-route", (req, res) => {
-  // if (req.isAuthenticated()) {
-    console.log("_______________________________________", req.user);
+  if (req.isAuthenticated()) {
     const {_id,name,email,rank,memberSince,totalPoints} = req.user;
     const userData = {
         _id,
@@ -69,13 +74,22 @@ app.get("/protected-route", (req, res) => {
     }
     console.log("User had active Session!");
     res.status(200).send(userData);
-  // } else {
+  } else {
     // console.log("User dont have active Session:", userData);
     res.status(401).json({ error: "User session not found" });
-  // }
+  }
 });
 
+
 app.use("/", route);
+
+const publicPath = join(__dirname, 'frontend', 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(publicPath, 'index.html'));
+});
 
 app.listen(8080, (err) => {
   if (err) {
